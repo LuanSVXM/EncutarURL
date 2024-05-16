e3e6aa826ccecf56f753420543e8b323d8c5f119
Luan Vieira <luansv123@gmail.com>
2024-05-16 00:24:26 -0300

    feat: add fist route in api

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express, { Router, Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+const RouterApi = Router();
+
+RouterApi.get("/", (request: Request, response: Response) =>  {
+    return response.status(200).json({message: "Seja bem vindo a api (/^▽^)/ "})
+});
+
+app.use(RouterApi);
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em: http://localhost:${port}`)
+});
