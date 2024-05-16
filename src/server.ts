119ebd017199538af3138e7930f1e76f8f9461c5
Luan Vieira <luansv123@gmail.com>
2024-05-16 02:11:51 -0300

    feat: add config env

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
-import express, { Router, Request, Response } from "express";
-import dotenv from 'dotenv';
-import cors from "cors";
-
+import dotenv from 'dotenv'
 dotenv.config();
+import express, { Router, Request, Response } from "express";
+import Config from './config';
+import cors from "cors";
+import { DataSource } from 'typeorm';
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+const port = Config().PORT;
+
 const RouterApi = Router();
+
+export const AppDataSource = new DataSource({...Config().DATASOURCE});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log(`Sucess connect database to schema ${process.env.DB_SCHEMA}`);
+  })
+  .catch((error) => console.log(error));
 
 RouterApi.get("/", (request: Request, response: Response) =>  {
     return response.status(200).json({message: "Seja bem vindo a api (/^▽^)/ "});
