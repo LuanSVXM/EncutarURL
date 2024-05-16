bc74dbaaf4dc6708adf42553056d96a88086c1f2
Luan Vieira <luansv123@gmail.com>
2024-05-16 01:37:17 -0300

    feat: add typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { Router, Request, Response } from "express";
+import dotenv from 'dotenv';
 import cors from "cors";
 
-const app = express();
+dotenv.config();
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
@@ -11,6 +13,10 @@ const RouterApi = Router();
 
 RouterApi.get("/", (request: Request, response: Response) =>  {
     return response.status(200).json({message: "Seja bem vindo a api (/^▽^)/ "});
+});
+
+RouterApi.get("/redirect", (request: Request, response: Response) =>  {
+    return response.status(301).redirect("https://www.google.com/");
 });
 
 app.use(RouterApi);
